2728f0ac8a78697972a248328187246ec610b816
Danny Nunez <114783191+Danny-Nunez@users.noreply.github.com>
2023-01-12 20:39:40 -0500

    Add files via upload

---
+++ server/config/constant.php
@@ -0,0 +1,20 @@
+<?php
+
+
+//status
+const ACTIVE = 'active';
+const INACTIVE = 'inactive';
+
+//management section
+
+const MANAGEMENT_SECTION = [
+    'dashboard_management' => 'dashboard_management',
+    'pos_management' => 'pos_management',
+    'order_management' => 'order_management',
+    'product_management' => 'product_management',
+    'business_management' => 'business_management',
+    'employee_management' => 'employee_management',
+    'deliveryman_management' => 'deliveryman_management',
+    'customer_management' => 'customer_management',
+    'report_management' => 'report_management',
+];
